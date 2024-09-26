841a29007af6a39e4d006bca191533ff75ae2b3b
boopathi186 <boopathi.selvaraj@concertidc.com>
2024-09-26 19:23:45 +0530

    correction in attendance

---
+++ attendance_management/src/main/java/com/attendance/attendance_management/Table/AttendanceInfo.java
@@ -0,0 +1,37 @@
+package com.attendance.attendance_management.Table;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Component
+@Entity
+@Getter
+@Setter
+@Table(name = "attendance_info")
+public class AttendanceInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int attendance_id;
+    @ManyToOne
+    @JoinColumn(name = "user_id",referencedColumnName ="user_id")
+    private UserDetails user;
+    private String date;
+    private String RecordIn;
+    private String RecordOut;
+    private String Status;
+
+    @Override
+    public String toString() {
+        return "Attendance_info{" +
+                "attendance_id=" + attendance_id +
+                ", user_id=" + user +
+                ", date='" + date + '\'' +
+                ", RecordIn='" + RecordIn + '\'' +
+                ", RecordOut='" + RecordOut + '\'' +
+                ", Status='" + Status + '\'' +
+                '}';
+    }
+}
